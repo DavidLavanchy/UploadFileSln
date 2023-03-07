7c51988b94aa7e302033c4504096ce98b6fb3249
David Lavanchy <dplavanchy@gmail.com>
2023-03-06 23:23:08 -0500

    Add form and bind file upload to form

---
+++ UploadFileApp/Data/CutstomerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace UploadFileApp.Data
+{
+    public class CutstomerModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = "dlavanchy";
+        public string? FirstName { get; set; } 
+        public string? LastName { get; set; } 
+        public string? FileName { get; set; } 
+
+    }
+}
